bf66143d2853fc93c52db4fd1aec2c480de3916b
sy <shiyung@gmail.com>
2022-09-04 01:38:34 +0800

    feat: first version of the pinned gist automation

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,15 @@ import table from "text-table";
 import Parser from "rss-parser";
 require('dotenv').config();
 
-const rssParser: Parser<> = new Parser();
+type feedData = {
+  title: string,
+}
+type itemData = {
+  title: string,
+  creator: string,
+}
+
+const rssParser: Parser<feedData, itemData> = new Parser();
 
 (async () => {
   const { GIST_ID, GH_TOKEN, WP_RSS_LINK } = process.env;
@@ -13,10 +21,26 @@ const rssParser: Parser<> = new Parser();
     return;
   }
 
+  const posts: string[][] = [];
   try {
     const res = await rssParser.parseURL(WP_RSS_LINK); 
+    console.log('info', res.title);
+    res.items.forEach(item => {
+      console.log('Item:', item.title, ' by ', item.creator);
+      posts.push([item.title, '']);
+    })
+
+    const gistTable = table(
+      [
+        [res.title, ''],
+        ['Lastest Articles', '👇'],
+        ...posts
+      ],
+      { align: ['l', 'r'], stringLength: () => 25}
+    );
+    const box = new GistBox({ id: GIST_ID, token: GH_TOKEN });
+    await box.update({ filename: 'wordpress-blog-latest.md', content: gistTable });
   } catch (err) {
-
+    throw new Error(`Cannot fetch the blog posts: ${err.message}`);
   }
-
 })();
